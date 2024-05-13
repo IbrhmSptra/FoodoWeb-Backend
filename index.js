5225451524cb73ca19bf6630cf377b7fe4e0c6b9
IbrhmSptra <sptraibrhm@gmail.com>
2024-05-13 11:04:34 +0700

    Fix : CORS origin

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const authRoutes = require("./src/routes/authRoutes");
 const cookieParser = require("cookie-parser");
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
